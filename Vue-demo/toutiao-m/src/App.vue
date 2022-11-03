332eb8d5a1de865ac93bc6ec30fd5a976089999b
bi-qiuqiu <121562382@qq.com>
2022-11-03 15:29:11 +0800

    toutiao

---
+++ Vue-demo/toutiao-m/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>头条头版</h1>
+    <van-button type="primary" class="try">主要按钮</van-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less" scoped>
+.try {
+  margin-top: 20px;
+}
+</style>
---
+++ Vue-demo/toutiao-m/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'amfe-flexible'
+import './styles/index.less'
+
+import { Button } from 'vant'
+
+Vue.use(Button)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
