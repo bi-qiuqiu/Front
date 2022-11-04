caa00a466339262f04e94ea19a12a8d00dbe5d4d
bi-qiuqiu <121562382@qq.com>
2022-11-04 10:33:47 +0800

    geek

--- React-项目练习/geek-pc/src/App.js
+++ React-项目练习/geek-pc/src/App.js
@@ -5,15 +5,16 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom'
 
-import Home from './pages/Layout'
-import Login from './pages/Login'
-import Notfound from './pages/NotFound'
+import Home from 'pages/Layout'
+import Login from 'pages/Login'
+import Notfound from 'pages/NotFound'
 function App() {
   return (
     <Router>
       <div className="App">
         <Link to="/home">首页</Link>
         <Link to="/login">登录</Link>
+        <hr/>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/home" component={Home} />
--- React-项目练习/geek-pc/src/pages/Login/index.js
+++ React-项目练习/geek-pc/src/pages/Login/index.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react'
-import { Card } from 'antd'
-import './index.css'
-import logo from '../../assets/images/logo.png'
+import { Card, Button, Checkbox, Form, Input } from 'antd'
+import './index.scss'
+import logo from 'assets/images/logo.png'
 class Login extends Component {
   state = {}
   render() {
     return (
       <div className="login">
         <Card className="login-container">
-          <img src={logo} alt="" className='login-logo'/>
-          <p>Card content</p>
-          <p>Card content</p>
+          <img src={logo} alt="" className="login-logo" />
+          <Form size="large">
+            <Form.Item
+              name="mobile"
+              rules={[
+                {
+                  required: true,
+                  message: '手机号不能为空!',
+                },
+                {
+                  pattern: /^1[3-9]\d{9}$/,
+                  message: '手机号码格式错误!',
+                },
+              ]}
+            >
+              <Input placeholder="请输入你的手机号" />
+            </Form.Item>
+
+            <Form.Item
+              name="cord"
+              rules={[
+                {
+                  required: true,
+                  message: '验证码不能为空!',
+                },
+                {
+                  pattern: /^\d{6}$/,
+                  message: '验证码码格式错误!',
+                },
+              ]}
+            >
+              <Input placeholder="请输入验证码" />
+            </Form.Item>
+
+            <Form.Item
+              valuePropName="checked"
+              name="agree"
+              rules={[
+                {
+                  required: true,
+                  message: '请同意协议',
+                },
+              ]}
+            >
+              <Checkbox>我已阅读并同意用户协议</Checkbox>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
         </Card>
       </div>
     )
